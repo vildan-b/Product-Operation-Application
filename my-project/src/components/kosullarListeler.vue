e30403862ea276c6ca718aeb952f10db67961097
vildan-b <vildanb@gmx.de>
2022-03-04 00:05:54 +0100

    test1

---
+++ my-project/src/components/kosullarListeler.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="exercise">
+
+    </div>
+</div>
+</template>
+
+<script>
+// eslint-disable-next-line
+/* eslint-disable */
+export default {
+  name: 'kosullarListeler',
+  methods: {
+},
+
+
+  data() {
+    return {
+    }
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
