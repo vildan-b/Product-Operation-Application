56abe75bd9cccb2db1d46c622e2c295904ed9d8a
vildan-b <vildanb@gmx.de>
2022-03-21 00:54:11 +0100

    Vue –Resource  “

--- bolum-19-test/src/main.js
+++ bolum-19-test/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import { router } from "./router"
 import { store } from "./store/store"
+import VueResource from "vue-resource"
+import VueRouter from 'vue-router'
+
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
--- bolum-19-test/src/store/store.js
+++ bolum-19-test/src/store/store.js
@@ -1,12 +1,23 @@
 import Vue from "vue"
 import Vuex from "vuex"
 import product from './modules/product'
+import *  as getters from "./getters"
+import *  as actions from "./actions"
+import *  as mutations from "./mutations"
+
 
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {},
+    state: {
+        purchase : 0.0,
+        sale: 0.0,
+        balance: 0.0
+    },
+    getters,
+    actions,
+    mutations,
     modules : {
         product
     }
