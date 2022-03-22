04838bb6c5c4be09081bff49ff78185972c182ee
vildan-b <vildanb@gmx.de>
2022-03-22 12:13:30 +0100

    Firebase Beginning“

--- bolum-19-test/src/store/modules/product.js
+++ bolum-19-test/src/store/modules/product.js
@@ -1,3 +1,4 @@
+import Vue from "vue";
 const state = {
 products : []
 }
@@ -20,8 +21,17 @@ const actions = {
   initApp( {commit}){
       // Vue Resource islemleri
   },
-  saveProduct( { commit} , payload){
+  saveProduct( {dispatch , commit , state} , product){
       // Vue Resource islemleri
+      Vue.http.post("https://product-operations-app-53bec-default-rtdb.firebaseio.com/product.json", product)
+      .then((response) => {
+          /***********  updating the product list*/
+        product.key = response.body.name;
+          commit("updateProductList" , product);
+          /******************  Updating the trading balance information */
+        dispatch
+      });
+
 
   },
   sellProduct({commit}, payload){
--- bolum-19-test/src/store/mutations.js
+++ bolum-19-test/src/store/mutations.js
@@ -1,3 +1,4 @@
 export const updateTradeResult = (state, payload) => {
     
+    
 }
